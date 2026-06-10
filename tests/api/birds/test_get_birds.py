03516a823787f71fd581b8b7d86e5d05049dbc90
user <lahunouanton@gmail.com>
2026-06-10 22:13:33 +0200

    Add filter tests for get api

--- tests/api/birds/test_get_birds.py
+++ tests/api/birds/test_get_birds.py
@@ -9,29 +9,29 @@ def test_get_birds_default_pagination(bird_client):
     assert isinstance(data, list)
     assert len(data) <= 20
 
-# def test_get_birds_with_pagination(bird_client):
-#     response = bird_client.get_birds(skip=0, limit=2)
-#     assert response.status_code == 200
-#     data = response.json()
-#     assert len(data) <= 2
-#
-# def test_get_birds_filter_by_species(bird_client):
-#     response = bird_client.get_birds(species="Parus major")
-#     assert response.status_code == 200
-#     data = response.json()
-#     if data:
-#         assert all(bird["species"] == "Parus major" for bird in data)
-#
-# def test_get_birds_filter_by_device_id(bird_client):
-#     response = bird_client.get_birds(device_id="emulator")
-#     assert response.status_code == 200
-#     data = response.json()
-#     if data:
-#         assert all(bird["device_id"] == "emulator" for bird in data)
-#
-# def test_get_birds_combined_filters(bird_client):
-#     response = bird_client.get_birds(species="Parus", skip=0, limit=5)
-#     assert response.status_code == 200
-#     data = response.json()
-#     assert isinstance(data, list)
-#     assert len(data) <= 5
+def test_get_birds_with_pagination(bird_client):
+    response = bird_client.get_birds(skip=0, limit=2)
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data) <= 2
+
+def test_get_birds_filter_by_species(bird_client):
+    response = bird_client.get_birds(species="Parus major")
+    assert response.status_code == 200
+    data = response.json()
+    if data:
+        assert all(bird["species"] == "Parus major" for bird in data)
+
+def test_get_birds_filter_by_device_id(bird_client):
+    response = bird_client.get_birds(device_id="emulator")
+    assert response.status_code == 200
+    data = response.json()
+    if data:
+        assert all(bird["device_id"] == "emulator" for bird in data)
+
+def test_get_birds_combined_filters(bird_client):
+    response = bird_client.get_birds(species="Parus", skip=0, limit=5)
+    assert response.status_code == 200
+    data = response.json()
+    assert isinstance(data, list)
+    assert len(data) <= 5
